b77cda0f335bf372e0ceae915106666a936b5150
Alyssa Morrow <akmorrow@berkeley.edu>
2019-01-24 10:25:45 -0800

    reads now working

--- mango-core/src/main/scala/org/bdgenomics/mango/converters/GA4GHutil.scala
+++ mango-core/src/main/scala/org/bdgenomics/mango/converters/GA4GHutil.scala
@@ -148,3 +148,62 @@ object GA4GHutil {
 
   }
 }
+
+case class SearchVariantsRequestGA4GH(variantSetId: String,
+                                      start: String,
+                                      end: String,
+                                      pageSize: String,
+                                      pageToken: String,
+                                      referenceName: String,
+                                      callSetIds: Array[String] = new Array[String](0),
+                                      binning: String = "1") {
+  def this(variantSetId: String,
+           start: String,
+           end: String,
+           pageSize: String,
+           pageToken: String,
+           referenceName: String,
+           callSetIds: Array[String]) = {
+    this(variantSetId: String,
+      start: String,
+      end: String,
+      pageSize: String,
+      pageToken: String,
+      referenceName: String,
+      callSetIds: Array[String],
+      "1")
+  }
+}
+
+case class SearchVariantsRequestGA4GHBinning(variantSetId: String,
+                                             start: String,
+                                             end: String,
+                                             pageSize: String,
+                                             pageToken: String,
+                                             referenceName: String,
+                                             callSetIds: Array[String] = new Array[String](0),
+                                             binning: String = "1") {
+  def this(variantSetId: String,
+           start: String,
+           end: String,
+           pageSize: String,
+           pageToken: String,
+           referenceName: String,
+           callSetIds: Array[String]) = {
+    this(variantSetId: String,
+      start: String,
+      end: String,
+      pageSize: String,
+      pageToken: String,
+      referenceName: String,
+      callSetIds: Array[String], "1")
+  }
+}
+
+// see proto defintiion: https://github.com/ga4gh/ga4gh-schemas/blob/master/src/main/proto/ga4gh/read_service.proto#L117
+case class SearchReadsRequestGA4GH(pageToken: String,
+                                   pageSize: Int,
+                                   readGroupIds: Array[String],
+                                   referenceId: String,
+                                   start: String,
+                                   end: String)
